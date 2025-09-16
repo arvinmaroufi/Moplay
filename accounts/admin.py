1752df939373982af73861b565cf813a3b1ce83f
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-16 07:15:57 +0430

    the users section in administration has been customized

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from . import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib import messages
 
-# Register your models here.
+
+# personalized actions
+def activate_users(modeladmin, request, queryset):
+    updated = queryset.update(is_active=True)
+    modeladmin.message_user(
+        request, _(f"{updated} کاربر با موفقیت فعال شدند."), messages.SUCCESS)
+activate_users.short_description = "فعال کردن کاربران انتخاب شده"
+
+
+def deactivate_users(modeladmin, request, queryset):
+    """Action برای غیرفعال کردن کاربران انتخاب شده"""
+    updated = queryset.update(is_active=False)
+    modeladmin.message_user(
+        request, _(f"{updated} کاربر با موفقیت غیرفعال شدند."), messages.SUCCESS)
+deactivate_users.short_description = "غیرفعال کردن کاربران انتخاب شده"
+
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        ('عمومی', {'fields': ('email', 'password')}),
+        ('اطلاعات شخصی', {'fields': ('first_name', 'last_name', 'phone', 'about_me', 'profile_photo')}),
+        ('اجازه ها', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        ('تاریخ های مهم', {'fields': ('last_login', 'date_joined')}),
+    )
+
+    add_fieldsets = (
+        ('عمومی', {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+        ('اطلاعات شخصی', {
+            'fields': ('first_name', 'last_name', 'phone', 'profile_photo')
+        }),
+        ('اجازه ها', {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')
+        }),
+    )
+
+    list_display = ['email', 'first_name', 'last_name', 'phone', 'is_staff', 'is_active']
+    search_fields = ['email', 'phone', 'first_name', 'last_name']
+    list_filter = ['is_staff', 'is_superuser', 'is_active', 'groups']
+    ordering = ['date_joined']
+    actions = [activate_users, deactivate_users]
