04d74396030913ba0ca5123733c9666ef1c4c290
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-17 03:55:36 +0430

    the register function was added to the accounts app views.py file

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import RegisterForm
+from .models import User
 
-# Create your views here.
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('core:home')
+
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            User.objects.create_user(
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password'],
+                first_name=form.cleaned_data['first_name'],
+                last_name=form.cleaned_data['last_name']
+            )
+            messages.success(request, 'حساب کاربری شما با موفقیت ایجاد شد')
+            return redirect('accounts:login')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{error}")
+    else:
+        form = RegisterForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/register.html', context)
