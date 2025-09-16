64945b7443068ebace63cdf375d7078cd95b63cd
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-17 03:28:01 +0430

    email settings added to settings.py

--- Moplay/settings.py
+++ Moplay/settings.py
@@ -125,6 +125,15 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 
 
+# Email settings (configure based on your email provider)
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'your_email@example.com'
+EMAIL_HOST_PASSWORD = 'your_email_password'
+
+
 # jalali date settings (optional)
 JALALI_DATE_DEFAULTS = {
    # if change it to true then all dates of the list_display will convert to the Jalali.
