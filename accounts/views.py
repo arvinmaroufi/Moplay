286efe2a526cdbbbe36414bd30bb3fdd420d5fb4
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-17 03:59:06 +0430

    the login function was added to the accounts app views.py file

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import RegisterForm
-from .models import User
+from .forms import RegisterForm, LoginForm
+from .models import User, VerificationCode
+from django.utils import timezone
+from .utils import send_verification_email
 
 
 def register(request):
@@ -30,3 +32,34 @@ def register(request):
         'form': form
     }
     return render(request, 'accounts/register.html', context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect('core:home')
+
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = form.cleaned_data['user']
+
+            request.session['pending_user_id'] = user.id
+            request.session['pending_user_email'] = user.email
+            request.session['pending_user_created_at'] = str(timezone.now())
+
+            verification_code = VerificationCode.generate_code(user)
+            send_verification_email(user, verification_code.code)
+
+            messages.info(request, 'کد تایید به ایمیل شما ارسال شد')
+            return redirect('accounts:verify_code')
+        else:
+            for field, errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, f"{error}")
+    else:
+        form = LoginForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'accounts/login.html', context)
