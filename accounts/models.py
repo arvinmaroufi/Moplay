661d766f4f15a23a1c6af581c873dafb49458b2e
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-16 05:25:09 +0430

    User model added to the accounts app and migrated to the db

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    username = None
+    phone = models.CharField(max_length=11, blank=True, null=True, verbose_name="شماره تلفن")
+    about_me = models.TextField(max_length=300, blank=True, null=True, verbose_name="درباره من")
+    profile_photo = models.ImageField(upload_to='profile_photos/', blank=True, null=True, verbose_name="عکس پروفایل")
+    email = models.EmailField(max_length=100, unique=True, blank=False, verbose_name='آدرس ایمیل')
+    is_active = models.BooleanField(default=True, verbose_name='فعال')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["first_name", "last_name"]
+
+    objects = UserManager()
+
+    class Meta:
+        verbose_name = 'کاربر'
+        verbose_name_plural = 'کاربران'
+        ordering = ['date_joined']
