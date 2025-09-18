e5b7283941ef52ac304774869fb5bdb59e68065f
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-18 07:47:32 +0430

    the url for the mark_notification_as_read function has been added to the dashboard app urls.py file

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,4 +6,5 @@ app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('notifications/<int:notification_id>/mark-as-read/', views.mark_notification_as_read, name='mark_notification_as_read'),
 ]
