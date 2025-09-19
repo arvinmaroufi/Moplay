9868941f98d6c897d9a1b6f531f24cc68e7bbb35
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-19 09:37:47 +0430

    Language model added to the movie app and migrated to the db

--- movie/models.py
+++ movie/models.py
@@ -29,3 +29,16 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=50, unique=True, verbose_name='زبان')
+    slug = models.SlugField(max_length=50, unique=True, verbose_name='نامک')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+
+    class Meta:
+        verbose_name = 'زبان'
+        verbose_name_plural = 'زبان ها'
+
+    def __str__(self):
+        return self.name
