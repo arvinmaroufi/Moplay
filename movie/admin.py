087caac34215b898f70056b8013d3a6b8667c175
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-19 11:12:40 +0430

    the genres section in administration has been customized

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
+from jalali_date import datetime2jalali
 
-# Register your models here.
+
+@admin.register(models.Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'get_created_at_jalali']
+    list_filter = ['created_at']
+    search_fields = ['title']
+
+    @admin.display(description='تاریخ ایجاد', ordering='created_at')
+    def get_created_at_jalali(self, obj):
+        return datetime2jalali(obj.created_at).strftime('%a، %d %b %Y')
