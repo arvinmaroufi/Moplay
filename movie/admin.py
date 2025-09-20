65a5eb66f452c43fc998b1981757c2585600ac0e
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-20 07:18:00 +0430

    the actors section in administration has been customized

--- movie/admin.py
+++ movie/admin.py
@@ -68,3 +68,14 @@ class DirectorAdmin(admin.ModelAdmin):
     @admin.display(description='تاریخ ایجاد', ordering='created_at')
     def get_created_at_jalali(self, obj):
         return datetime2jalali(obj.created_at).strftime('%a، %d %b %Y')
+
+
+@admin.register(models.Actor)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'get_created_at_jalali']
+    list_filter = ['created_at']
+    search_fields = ['name']
+
+    @admin.display(description='تاریخ ایجاد', ordering='created_at')
+    def get_created_at_jalali(self, obj):
+        return datetime2jalali(obj.created_at).strftime('%a، %d %b %Y')
