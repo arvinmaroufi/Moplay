7048b939f605a78bcacc0d6602e75bf74a87b4c1
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-20 07:27:29 +0430

    the movies section in administration has been customized

--- movie/admin.py
+++ movie/admin.py
@@ -85,3 +85,41 @@ class MovieCommentInline(admin.TabularInline):
     model = models.MovieComment
     extra = 0
     readonly_fields = ['created_at']
+
+
+@admin.register(models.Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'dubbed_or_subtitle', 'subscription_status', 'status', 'views', 'get_created_at_jalali']
+    list_filter = ['status', 'dubbed_or_subtitle', 'subscription_status', 'created_at', 'genre', 'country']
+    search_fields = ['title', 'description']
+    prepopulated_fields = {'slug': ('title',)}
+    filter_horizontal = ['genre', 'tags', 'language', 'actors', 'similar_movies', 'country', 'director']
+    readonly_fields = ['created_at', 'updated_at']
+    inlines = [MovieCommentInline]
+    fieldsets = (
+        ('اطلاعات اصلی', {
+            'fields': ('title', 'slug', 'description', 'vertical_banner', 'horizontal_banner')
+        }),
+        ('دسته‌بندی و برچسب‌ها', {
+            'fields': ('genre', 'tags', 'language', 'country')
+        }),
+        ('افراد مرتبط', {
+            'fields': ('actors', 'director', 'similar_movies')
+        }),
+        ('مشخصات فنی', {
+            'fields': ('duration', 'release_date', 'score', 'age_range')
+        }),
+        ('لینک‌های رسانه', {
+            'fields': ('trailer', 'subtitle_url', 'quality_480p', 'quality_720p', 'quality_1080p')
+        }),
+        ('تنظیمات نمایش', {
+            'fields': ('dubbed_or_subtitle', 'subscription_status', 'is_recommended', 'status')
+        }),
+        ('آمار و تاریخ‌ها', {
+            'fields': ('views', 'created_at', 'updated_at')
+        }),
+    )
+
+    @admin.display(description='تاریخ ایجاد', ordering='created_at')
+    def get_created_at_jalali(self, obj):
+        return datetime2jalali(obj.created_at).strftime('%a، %d %b %Y')
