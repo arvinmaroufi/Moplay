ab6be3b60a8026b76a49ecb82bcc2c8216e8bb75
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-22 15:49:29 +0330

    the duration_format function was added to the custom_filters.py file

---
+++ movie/templatetags/custom_filters.py
@@ -0,0 +1,25 @@
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter(name='duration_format')
+def duration_format(value):
+    try:
+        minutes = int(value)
+    except (ValueError, TypeError):
+        return
+
+    if minutes <= 0:
+        return
+
+    hours = minutes // 60
+    remaining_minutes = minutes % 60
+
+    if hours > 0 and remaining_minutes > 0:
+        return f"{hours} ساعت {remaining_minutes} دقیقه"
+    elif hours > 0:
+        return f"{hours} ساعت"
+    else:
+        return f"{minutes} دقیقه"
