61b02030b378845913ed379ca514db57ae95dae8
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-22 15:36:06 +0330

    the get_pages_to_show function was added to the movie app views.py file

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_pages_to_show(current_page, total_pages):
+    if total_pages <= 3:
+        return list(range(1, total_pages + 1))
+
+    if current_page <= 2:
+        return [1, 2, 3, '...', total_pages]
+
+    if current_page >= total_pages - 1:
+        return [1, '...', total_pages - 2, total_pages - 1, total_pages]
+
+    return [1, '...', current_page - 1, current_page, current_page + 1, '...', total_pages]
