ffc14f1a05d0cc50fbc5d120c671d563d4c90ecd
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-22 15:59:09 +0330

    the series_list function was added to the movie app views.py file

--- movie/views.py
+++ movie/views.py
@@ -88,3 +88,76 @@ def movies_list(request):
         'current_filters': request.GET,
     }
     return render(request, 'movie/movies_list.html', context)
+
+
+def series_list(request):
+    series = Series.objects.filter(status='published').order_by('-views')
+
+    # filters
+
+    # filter by search
+    search_query = request.GET.get('search', '')
+    if search_query:
+        series = series.filter(Q(title__icontains=search_query))
+
+    # filter by genre
+    genre_filter = request.GET.getlist('genre')
+    if genre_filter:
+        series = series.filter(genre__id__in=genre_filter).distinct()
+
+    # filter by year
+    year_filter = request.GET.getlist('year')
+    if year_filter:
+        series = series.filter(release_date__year__in=year_filter)
+
+    # filter by country
+    country_filter = request.GET.getlist('country')
+    if country_filter:
+        series = series.filter(country__id__in=country_filter).distinct()
+
+    # filter by dubbing/subtitle status
+    dubbed_filter = request.GET.get('dubbed_or_subtitle', '')
+    if dubbed_filter:
+        series = series.filter(dubbed_or_subtitle=dubbed_filter)
+
+    # filter by subscription status
+    subscription_filter = request.GET.get('subscription_status', '')
+    if subscription_filter:
+        series = series.filter(subscription_status=subscription_filter)
+
+    # sorting
+    sort_by = request.GET.get('sort', '')
+    if sort_by == 'newest':
+        series = series.order_by('-created_at')
+    elif sort_by == 'oldest':
+        series = series.order_by('created_at')
+    elif sort_by == 'highest_views':
+        series = series.order_by('-views')
+    elif sort_by == 'highest_score':
+        series = series.order_by('-score')
+
+    # pagination
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(series, 15)
+    try:
+        object_list = paginator.page(page_number)
+    except PageNotAnInteger:
+        object_list = paginator.page(1)
+    except EmptyPage:
+        object_list = paginator.page(paginator.num_pages)
+    pages_to_show = get_pages_to_show(object_list.number, paginator.num_pages)
+
+    # Get all genres, years, and countries to display in the filter
+    all_genres = Genre.objects.all()
+    all_years = Year.objects.all().order_by('-year')
+    all_countries = Country.objects.all()
+
+    context = {
+        'series': object_list,
+        'pages_to_show': pages_to_show,
+        'all_genres': all_genres,
+        'all_years': all_years,
+        'all_countries': all_countries,
+        'current_filters': request.GET,
+    }
+    return render(request, 'movie/series_list.html', context)
