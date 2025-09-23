85871f83c8fd9464967aba0b5726c134f74c6122
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-23 23:24:57 +0330

    the signals file was called in the apps.py file in the movie app

--- movie/apps.py
+++ movie/apps.py
@@ -5,3 +5,6 @@ class MovieConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movie'
     verbose_name = 'فیلم و سریال'
+
+    def ready(self):
+        import movie.signals
