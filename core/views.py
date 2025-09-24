261db56b6deee15826b7522a9fdf69b575dc728f
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-24 04:38:08 +0330

    receiving top-rated movies and series in the home function in the core app was done

--- core/views.py
+++ core/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from movie.models import Movie, Series
+from itertools import chain
+from operator import attrgetter
 
 
 def home(request):
-    return render(request, 'core/home.html')
+    movies = Movie.objects.filter(status='published')
+    series = Series.objects.filter(status='published')
+    contents = sorted(
+        chain(movies, series),
+        key=attrgetter('created_at'),
+        reverse=True
+    )
+    top_rated_contents = sorted(contents, key=lambda x: float(x.score.split('/')[0]), reverse=True)[:3]
+
+    context = {
+        'top_rated_contents': top_rated_contents
+    }
+    return render(request, 'core/home.html', context)
