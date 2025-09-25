39e029dd1d6a2b8d2a449840f2837e2fa383d09b
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-25 21:28:58 +0330

    the genre_series function was added to the movie app views.py file

--- movie/views.py
+++ movie/views.py
@@ -296,3 +296,26 @@ def genre_movies(request, slug):
         'pages_to_show': pages_to_show,
     }
     return render(request, 'movie/genre_movies.html', context)
+
+
+def genre_series(request, slug):
+    genre = get_object_or_404(Genre, slug=slug)
+    series = genre.series_genre.filter(status='published')
+
+    # pagination
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(series, 15)
+    try:
+        object_list = paginator.page(page_number)
+    except PageNotAnInteger:
+        object_list = paginator.page(1)
+    except EmptyPage:
+        object_list = paginator.page(paginator.num_pages)
+    pages_to_show = get_pages_to_show(object_list.number, paginator.num_pages)
+
+    context = {
+        'genre': genre,
+        'series': object_list,
+        'pages_to_show': pages_to_show,
+    }
+    return render(request, 'movie/genre_series.html', context)
