bfb9bfa2b49613172b30cbadd1f9abe0b09f216f
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-09-27 05:57:10 +0330

    the SocialMediaAdminForm class was added to the forms.py file in the movie app

--- movie/forms.py
+++ movie/forms.py
@@ -2,6 +2,23 @@ from django import forms
 from . import models
 
 
+class SocialMediaAdminForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        social_media_fields = {
+            'twitter_url': 'https://twitter.com/username',
+            'instagram_url': 'https://instagram.com/username',
+            'facebook_url': 'https://facebook.com/username',
+        }
+
+        for field_name, placeholder in social_media_fields.items():
+            if field_name in self.fields:
+                self.fields[field_name].widget.attrs.update({
+                    'placeholder': placeholder,
+                    'class': 'vURLField'
+                })
+
+
 class DirectorAdminForm(forms.ModelForm):
     class Meta:
         model = models.Director
