e0eddd02c56a23458f75fde6c718b875a840e52c
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-01 03:21:53 +0330

    the actors_list function was added to the movie app views.py file

--- movie/views.py
+++ movie/views.py
@@ -359,3 +359,23 @@ def director_detail(request, slug):
         'contents': contents,
     }
     return render(request, 'movie/director_detail.html', context)
+
+
+def actors_list(request):
+    actors = Actor.objects.all().order_by('-created_at')
+
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(actors, 15)
+    try:
+        object_list = paginator.page(page_number)
+    except PageNotAnInteger:
+        object_list = paginator.page(1)
+    except EmptyPage:
+        object_list = paginator.page(paginator.num_pages)
+    pages_to_show = get_pages_to_show(object_list.number, paginator.num_pages)
+
+    context = {
+        'actors': object_list,
+        'pages_to_show': pages_to_show,
+    }
+    return render(request, 'movie/actors_list.html', context)
