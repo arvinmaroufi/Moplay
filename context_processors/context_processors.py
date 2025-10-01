8ad5ec4d71832a4e0dcfa5ecd8289d2d566122fe
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-01 05:44:25 +0330

    a query to retrieve the top 20 popular tags sorted by views was added to the movie_func function in the context_processors.py file

--- context_processors/context_processors.py
+++ context_processors/context_processors.py
@@ -1,11 +1,13 @@
-from movie.models import Genre
+from movie.models import Genre, Tag
 
 
 def movie_func(request):
     genres_movies = Genre.objects.all()
     genres_series = Genre.objects.all()
+    popular_tags = Tag.objects.all().order_by('-views')[:20]
 
     return {
         'genres_movies': genres_movies,
         'genres_series': genres_series,
+        'popular_tags': popular_tags,
     }
