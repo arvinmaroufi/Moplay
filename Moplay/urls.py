0462163b834cf87abf0d7f50838aa4f5ce5328d6
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-06 23:02:24 +0330

    added redirect for country url to home in project urls.py file

--- Moplay/urls.py
+++ Moplay/urls.py
@@ -18,5 +18,6 @@ urlpatterns = [
     path('tag/', redirect_to_home, name='redirect_to_home'),
     path('language/', redirect_to_home, name='redirect_to_home'),
     path('year/', redirect_to_home, name='redirect_to_home'),
+    path('country/', redirect_to_home, name='redirect_to_home'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
