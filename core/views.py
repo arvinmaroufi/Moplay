bbc976429e0bf95887f49c0ba572eed0439eefcb
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-06 23:14:13 +0330

    the get_pages_to_show function was added to the core app views.py file

--- core/views.py
+++ core/views.py
@@ -8,6 +8,19 @@ def redirect_to_home(request):
     return redirect('core:home')
 
 
+def get_pages_to_show(current_page, total_pages):
+    if total_pages <= 3:
+        return list(range(1, total_pages + 1))
+
+    if current_page <= 2:
+        return [1, 2, 3, '...', total_pages]
+
+    if current_page >= total_pages - 1:
+        return [1, '...', total_pages - 2, total_pages - 1, total_pages]
+
+    return [1, '...', current_page - 1, current_page, current_page + 1, '...', total_pages]
+
+
 def home(request):
     movies = Movie.objects.filter(status='published')
     series = Series.objects.filter(status='published')
