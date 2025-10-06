423375e1bc86f4e23356908d4d19aa0138c19e91
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-06 22:56:22 +0330

    added redirect for genre url to home in project urls.py file

--- Moplay/urls.py
+++ Moplay/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from . import settings
+from core.views import redirect_to_home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +10,8 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('dashboard/', include('dashboard.urls')),
     path('', include('movie.urls')),
+
+    # redirect urls
+    path('genre/', redirect_to_home, name='redirect_to_home')
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
