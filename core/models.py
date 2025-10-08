dccd4461dea1d4b849e079976de8be2da9c222ca
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-08 22:23:10 +0330

    Contact model added to the core app and migrated to the db

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100, verbose_name='نام')
+    email = models.EmailField(max_length=200, verbose_name='ایمیل')
+    subject = models.CharField(max_length=100, verbose_name='موضوع')
+    message = models.TextField(max_length=3000, verbose_name='متن پیام')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ارسال')
+
+    class Meta:
+        verbose_name = "پیام"
+        verbose_name_plural = "پیام ها"
+
+    def __str__(self):
+        return f'{self.email} - {self.subject}'
