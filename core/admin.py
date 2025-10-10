b25c3042dd597b401eab4658cd5b5861016cbb6d
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-10 03:33:54 +0330

    the faq section in administration has been customized

--- core/admin.py
+++ core/admin.py
@@ -19,3 +19,24 @@ class ContactAdmin(admin.ModelAdmin):
             return obj.subject[:50] + '...'
         return obj.subject
     short_subject.short_description = 'موضوع'
+
+
+@admin.register(models.FAQ)
+class FaqAdmin(admin.ModelAdmin):
+    list_display = ['short_question', 'short_answer', 'get_created_at_jalali']
+
+    @admin.display(description='تاریخ ایجاد', ordering='created_at')
+    def get_created_at_jalali(self, obj):
+        return datetime2jalali(obj.created_at).strftime('%a، %d %b %Y')
+
+    def short_question(self, obj):
+        if len(obj.question) > 40:
+            return obj.question[:40] + '...'
+        return obj.question
+    short_question.short_description = 'سوال'
+
+    def short_answer(self, obj):
+        if len(obj.answer) > 40:
+            return obj.answer[:40] + '...'
+        return obj.answer
+    short_answer.short_description = 'جواب'
