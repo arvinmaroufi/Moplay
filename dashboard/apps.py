d8dcf0d8cd4caf83c4c8763cf71e14c2a82e849e
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-10 05:10:05 +0330

    the signals file was called in the apps.py file in the dashboard app

--- dashboard/apps.py
+++ dashboard/apps.py
@@ -5,3 +5,6 @@ class DashboardConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'dashboard'
     verbose_name = 'داشبورد'
+
+    def ready(self):
+        import dashboard.signals
