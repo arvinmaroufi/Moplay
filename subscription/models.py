ce98f390191c884341bfe2441090cebe6b9b8577
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-12 23:33:30 +0330

    UserSubscription model added to the subscription app and migrated to the db

--- subscription/models.py
+++ subscription/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from accounts.models import User
+from django.utils import timezone
+import uuid
 
 
 class SubscriptionPlan(models.Model):
@@ -30,3 +33,45 @@ class SubscriptionPlan(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.price} تومان"
+
+
+class UserSubscription(models.Model):
+    STATUS_CHOICES = [
+        ('active', 'فعال'),
+        ('expired', 'منقضی شده'),
+        ('canceled', 'لغو شده'),
+        ('pending', 'در انتظار پرداخت'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='کاربر')
+    plan = models.ForeignKey(SubscriptionPlan, on_delete=models.CASCADE, verbose_name='پلن')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending', verbose_name='وضعیت')
+    start_date = models.DateTimeField(null=True, blank=True, verbose_name='تاریخ شروع')
+    end_date = models.DateTimeField(null=True, blank=True, verbose_name='تاریخ پایان')
+    payment_amount = models.PositiveIntegerField(verbose_name='مبلغ پرداختی')
+    transaction_id = models.UUIDField(default=uuid.uuid4, unique=True, verbose_name='شناسه تراکنش')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='تاریخ به‌روزرسانی')
+
+    class Meta:
+        verbose_name = 'اشتراک کاربر'
+        verbose_name_plural = 'اشتراک‌ های کاربران'
+
+    def __str__(self):
+        return self.user.email
+
+    def is_active(self):
+        if self.status == 'active' and self.end_date and self.end_date > timezone.now():
+            return True
+        return False
+
+    def save(self, *args, **kwargs):
+        if self.status == 'active' and self.end_date and self.end_date <= timezone.now():
+            self.status = 'expired'
+
+        if self.status == 'active' and not self.start_date:
+            self.start_date = timezone.now()
+            if self.plan.duration_days:
+                self.end_date = self.start_date + timezone.timedelta(days=self.plan.duration_days)
+
+        super().save(*args, **kwargs)
