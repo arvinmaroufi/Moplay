2bcf040d48a614db947de8ca86102c02abe84705
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-13 04:08:45 +0330

    the subscription_plans function was added to the subscription app views.py file

--- subscription/views.py
+++ subscription/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import SubscriptionPlan, UserSubscription
 
-# Create your views here.
+
+def subscription_plans(request):
+    plans = SubscriptionPlan.objects.filter(is_active=True)
+
+    context = {
+        'plans': plans,
+        'active_plan': None
+    }
+
+    if request.user.is_authenticated:
+        active_subscription = UserSubscription.objects.filter(
+            user=request.user,
+            status='active',
+            end_date__gt=timezone.now()
+        ).first()
+        context['active_plan'] = active_subscription.plan if active_subscription else None
+
+    return render(request, 'subscription/plans.html', context)
