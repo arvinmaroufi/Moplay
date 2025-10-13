2da270f97f2dc6e1e8653aeb7a9d162d73316fa9
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-13 04:30:11 +0330

    the url for the select_plan function has been added to the subscription app urls.py file

--- subscription/urls.py
+++ subscription/urls.py
@@ -6,4 +6,5 @@ app_name = 'subscription'
 
 urlpatterns = [
     path('plans/', views.subscription_plans, name='plans'),
+    path('select/<int:plan_id>/', views.select_plan, name='select_plan'),
 ]
