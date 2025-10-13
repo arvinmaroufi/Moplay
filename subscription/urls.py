22abae8e987e24dd89c9edec0dfd5eee8712250a
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-13 04:35:00 +0330

    the url for the user_subscriptions function has been added to the subscription app urls.py file

--- subscription/urls.py
+++ subscription/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('plans/', views.subscription_plans, name='plans'),
     path('select/<int:plan_id>/', views.select_plan, name='select_plan'),
     path('subscription/<int:subscription_id>/', views.subscription_detail, name='subscription_detail'),
+    path('my-subscriptions/', views.user_subscriptions, name='user_subscriptions'),
 ]
