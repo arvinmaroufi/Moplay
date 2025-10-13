03666bbc7221a4f8a444c2ae2e2b8dbad9eb4d37
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-13 22:46:19 +0330

    improved the check_content_access function in the decorators.py file in the movie app

--- movie/decorators.py
+++ movie/decorators.py
@@ -1,19 +1,25 @@
 from django.shortcuts import redirect
 from functools import wraps
+from django.utils import timezone
+from django.contrib import messages
 
 
 def check_content_access(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
+        # check for the existence of a slug for a movie or series
         content_slug = kwargs.get('slug') or kwargs.get('series_slug')
 
         if content_slug:
             try:
                 from .models import Movie, Series
+                from subscription.models import UserSubscription
+                # first we check if it is a movie
                 try:
                     content = Movie.objects.get(slug=content_slug)
                     content_type = 'movie'
                 except Movie.DoesNotExist:
+                    # if it weren't for the movie, we'd check out the series
                     try:
                         content = Series.objects.get(slug=content_slug)
                         content_type = 'series'
@@ -21,10 +27,25 @@ def check_content_access(view_func):
                         content = None
 
                 if content:
+                    # checking the status of the content
                     if content.subscription_status == 'subscription':
-                        return redirect('core:home')
+                        # check if the user is logged in
+                        if not request.user.is_authenticated:
+                            return redirect('accounts:login')
 
-            except Exception:
+                        # check the user's active subscription
+                        active_subscription = UserSubscription.objects.filter(
+                            user=request.user,
+                            status='active',
+                            end_date__gt=timezone.now()
+                        ).exists()
+
+                        if not active_subscription:
+                            messages.error(request, 'برای دسترسی به این محتوا باید اشتراک فعال داشته باشید')
+                            return redirect('subscription:plans')
+
+            except Exception as e:
+                # if an error occurs, it will redirect to the home page
                 return redirect('core:home')
 
         return view_func(request, *args, **kwargs)
