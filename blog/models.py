0550bc521604a524373e4a9507487948c0023217
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-15 06:21:46 +0330

    Tag model added to the blog app and migrated to the db

--- blog/models.py
+++ blog/models.py
@@ -12,3 +12,17 @@ class Category(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, unique=True, verbose_name='عنوان برچسب')
+    slug = models.SlugField(max_length=50, unique=True, verbose_name='نامک')
+    views = models.PositiveIntegerField(default=0, verbose_name='بازدید ها')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+
+    class Meta:
+        verbose_name = 'برچسب'
+        verbose_name_plural = 'برچسب ها'
+
+    def __str__(self):
+        return self.title
