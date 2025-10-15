b72110e6379cbe4ae3280009158588d62910fa1b
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-15 06:32:45 +0330

    the category section in administration in the blog app has been customized

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
+from jalali_date import datetime2jalali
 
-# Register your models here.
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'get_created_at_jalali']
+
+    @admin.display(description='تاریخ ایجاد', ordering='created_at')
+    def get_created_at_jalali(self, obj):
+        return datetime2jalali(obj.created_at).strftime('%a، %d %b %Y')
