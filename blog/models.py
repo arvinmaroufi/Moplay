bd469190681594d2cf72afb4f0ab7a4c508ae789
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-15 06:20:33 +0330

    Category model added to the blog app and migrated to the db

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=50, unique=True, verbose_name='عنوان دسته بندی')
+    slug = models.SlugField(max_length=50, unique=True, verbose_name='نامک')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ ایجاد')
+
+    class Meta:
+        verbose_name = 'دسته بندی'
+        verbose_name_plural = 'دسته بندی ها'
+
+    def __str__(self):
+        return self.title
