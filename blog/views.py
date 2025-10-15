292d03537dea6d006c955c3ec91151c411cb2f55
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-16 03:29:04 +0330

    the article_list function was added to the blog app views.py file

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Article
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def get_pages_to_show(current_page, total_pages):
@@ -12,3 +14,24 @@ def get_pages_to_show(current_page, total_pages):
         return [1, '...', total_pages - 2, total_pages - 1, total_pages]
 
     return [1, '...', current_page - 1, current_page, current_page + 1, '...', total_pages]
+
+
+def article_list(request):
+    articles = Article.objects.filter(status='published').order_by('-views')
+
+    # pagination
+    page_number = request.GET.get('page', 1)
+    paginator = Paginator(articles, 9)
+    try:
+        object_list = paginator.page(page_number)
+    except PageNotAnInteger:
+        object_list = paginator.page(1)
+    except EmptyPage:
+        object_list = paginator.page(paginator.num_pages)
+    pages_to_show = get_pages_to_show(object_list.number, paginator.num_pages)
+
+    context = {
+        'articles': object_list,
+        'pages_to_show': pages_to_show,
+    }
+    return render(request, 'blog/article_list.html', context)
