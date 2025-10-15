b0845e2106cc22d8fa5d0bf0b41370a281793c43
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-15 06:46:25 +0330

    the tag section in administration in the blog app has been customized

--- blog/admin.py
+++ blog/admin.py
@@ -10,3 +10,12 @@ class CategoryAdmin(admin.ModelAdmin):
     @admin.display(description='تاریخ ایجاد', ordering='created_at')
     def get_created_at_jalali(self, obj):
         return datetime2jalali(obj.created_at).strftime('%a، %d %b %Y')
+
+
+@admin.register(models.Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['title', 'views', 'get_created_at_jalali']
+
+    @admin.display(description='تاریخ ایجاد', ordering='created_at')
+    def get_created_at_jalali(self, obj):
+        return datetime2jalali(obj.created_at).strftime('%a، %d %b %Y')
