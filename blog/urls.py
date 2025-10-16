c109ad57cb4ca4018eb9200adb5414b9dd0388e4
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-16 04:10:46 +0330

    the url for the article_detail function has been added to the blog app urls.py file

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 
 urlpatterns = [
     path('articles/', views.article_list, name='article_list'),
+    path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
 ]
