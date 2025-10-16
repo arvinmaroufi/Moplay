545e964e205a0926f9ae43c870cdc9bdc535f461
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-16 06:00:14 +0330

    the url for the tag_articles function has been added to the blog app urls.py file

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('articles/', views.article_list, name='article_list'),
     path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
     path('articles/category/<slug:slug>/', views.category_articles, name='category_articles'),
+    path('articles/tag/<slug:slug>/', views.tag_articles, name='tag_articles'),
 ]
