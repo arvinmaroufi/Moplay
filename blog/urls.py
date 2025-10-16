d6dd4a66452ac30f3e70550c817cf7f6ee448cad
arvinmaroufi <arvinmaroufi.dev@gmail.com>
2025-10-16 05:46:20 +0330

    the url for the category_articles function has been added to the blog app urls.py file

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ app_name = 'blog'
 urlpatterns = [
     path('articles/', views.article_list, name='article_list'),
     path('articles/<slug:slug>/', views.article_detail, name='article_detail'),
+    path('articles/category/<slug:slug>/', views.category_articles, name='category_articles'),
 ]
